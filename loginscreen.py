98e93553a31528afaa8a2cc7b7bd45c51bdce271
Hanna Koh <hjk5643@g.rit.edu>
2022-10-14 19:55:42 -0400

    switching to kv file

--- loginscreen.py
+++ loginscreen.py
@@ -3,34 +3,35 @@ from tkinter import CENTER, Grid
 from typing import Text
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
 
+class LoginScreen(Widget):
+    pass
 
-class LoginScreen(GridLayout):
-
-    def __init__(self, **kwargs):
+class LoginScreenApp(GridLayout):
+    def build(self): 
+        return LoginScreen()
+    '''def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
         self.cols = 1
-        alayout = AnchorLayout(anchor_x = 'center', anchor_y = 'center')
+        a = FloatLayout()
         
-        glayout = GridLayout(cols = 1)
-        glayout.add_widget(Label(text='User Name'))
-        glayout.username = TextInput(multiline = False)
-        glayout.add_widget(glayout.username)
-        glayout.add_widget(Label(text='Password'))
-        glayout.password = TextInput(password=True, multiline=False)
-        glayout.add_widget(glayout.password)
-        alayout.add_widget(glayout)
-        self.add_widget(alayout)
+        a.glayout = GridLayout(cols = 1, size_hint=(.3, .2),
+                pos_hint={'x':.35, 'y':.4})
+        a.glayout.username = TextInput(text = 'Username', multiline = False)
+        a.glayout.add_widget(a.glayout.username)
+        a.glayout.password = TextInput(text = 'Password', password=True, multiline=False)
+        a.glayout.add_widget(a.glayout.password)
+        a.add_widget(a.glayout)
+        self.add_widget(a)'''
+        
 
 
-class MyApp(App):
-
-    def build(self):
-        return LoginScreen()
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    LoginScreenApp().run()
