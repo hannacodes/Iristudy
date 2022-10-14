57a67c3cd088300b1a37de479cc144db843a4d86
tusmm <urbadxd1234@gmail.com>
2022-10-14 19:36:22 -0400

    initial app setup

--- creategroup.py
+++ creategroup.py
@@ -3,3 +3,13 @@ Create group screen
 form like with subject, set a random group id,
 ask for group name..
 """
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text="Create group")
+
+if __name__ == "__main__":
+    MyApp().run()
