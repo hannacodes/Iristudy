72b371ccd569e4aba2f7ac36a745f874dabd1e51
lh5844 <lh5844rit@gmail.com>
2022-10-15 02:20:12 -0400

    basic framework for mygroup screen with scrollview

--- mygroups.py
+++ mygroups.py
@@ -6,10 +6,12 @@ Window.size = (480/1.5, 853/1.5)
 
 from kivy.app import App 
 from kivy.uix.widget import Widget 
+from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 
 class scrollingtest(BoxLayout):
     pass
+            
 
 class mygroups(Widget): 
     pass 
