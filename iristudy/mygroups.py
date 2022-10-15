ee6094e9d2aae30b2504d992e3d18efa98fd0944
lh5844 <lh5844rit@gmail.com>
2022-10-15 13:01:34 -0400

    font and color formatted mygroups and homepage

--- iristudy/mygroups.py
+++ iristudy/mygroups.py
@@ -5,23 +5,15 @@ from kivy.core.window import Window
 Window.size = (480/1.5, 853/1.5)
 
 from kivy.app import App 
-from kivy.uix.widget import Widget 
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.screenmanager import Screen
 from kivy.graphics import *
-
-class MyRelativeLayout(RelativeLayout):
-    def adjust_size(self, *args):
-        self.rect.size = self.size  # set the size of the Rectangle
-
-
-
+from kivy.utils import *
 
 class Scroll(ScrollView):
     def __init__(self,  **kwargs):
@@ -30,13 +22,12 @@ class Scroll(ScrollView):
         layout.bind(minimum_height=layout.setter('height'))
         # Make sure the height is such that there is something to scroll.
         for i in range(15):
-            #SkillStats = BoxLayout(size_hint_y = None, orientation = "vertical")
             SkillStat = BoxLayout(spacing = 2, height = 150, orientation = "vertical", size_hint_y = None)
-            SkillStat.add_widget(Label(text = "Name", color = (0,0,0,1), size_hint = (None, None), size = (80, 20), pos_hint = {"center_x":.5, "top":0}))
-            SkillStat.add_widget(Label(text = "Subject", color = (0,0,0,1), size_hint = (None, None), size = (80, 20), pos_hint = {"center_x":.5, "top":0}))
-            SkillStat.add_widget(Label(text = "Admin", color = (0,0,0,1), size_hint = (None, None), size = (80, 20), pos_hint = {"center_x":.5, "top":0}))
+            SkillStat.add_widget(Label(text = "Name", color = (0,0,0,1), size_hint = (None, None), size = (80, 20), pos_hint = {"center_x":.5, "top":0}, font_name = 'assets/fonts/static/Fredoka/Fredoka-regular'))
+            SkillStat.add_widget(Label(text = "Subject", color = (0,0,0,1), size_hint = (None, None), size = (80, 20), pos_hint = {"center_x":.5, "top":0}, font_name = 'assets/fonts/static/Fredoka/Fredoka-regular'))
+            SkillStat.add_widget(Label(text = "Admin", color = (0,0,0,1), size_hint = (None, None), size = (80, 20), pos_hint = {"center_x":.5, "top":0}, font_name = 'assets/fonts/static/Fredoka/Fredoka-regular'))
             flaylout = FloatLayout(size_hint = (1, 1))
-            flaylout.add_widget(Button(text = "more info", size_hint = (0.3, 0.4), pos_hint = {"center_x": .5, "top": 0.9}))
+            flaylout.add_widget(Button(text = "more info", size_hint = (0.3, 0.4), pos_hint = {"center_x": .5, "top": 0.9}, font_name = 'assets/fonts/static/Fredoka/Fredoka-regular', background_color = get_color_from_hex('#BF98D1'), background_normal = ''))
             SkillStat.add_widget(flaylout)
             layout.add_widget(SkillStat)
 
