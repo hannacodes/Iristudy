07fdbf1103ac59039d32dd939973c083e5376ef2
Hanna Koh <hjk5643@g.rit.edu>
2022-10-15 13:26:04 -0400

    calendar kinda working

--- iristudy/creategroup.py
+++ iristudy/creategroup.py
@@ -4,6 +4,8 @@ form like with subject, set a random group id,
 ask for group name..
 """
 
+from decimal import Clamped
+from turtle import onclick
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label # writing
@@ -38,15 +40,32 @@ class CreateFormScreen(Screen):
         self.groupName.text = ""
         self.description.text = ""
 
-
 class Pop(FloatLayout):
-    pass
+    def __init__(self, **kwargs):
+        super(FloatLayout, self).__init__(**kwargs)
+        glay = GridLayout(cols=7, size_hint = (0.5, 0.5))
+        for __ in range(56): 
+            glay.add_widget(Button(text="t", background_color= (0,0,0,0.5)))
+        self.add_widget(glay)
+class NewBtn(Button):
+    def __init__(self, **kwargs):
+        super(Button, self).__init__(**kwargs)
+
+    def changebg(a,b): 
+        a.background_color = (1.2, 1.2, 1, 0)
 
 def show_popup():
-    show = Pop()
+    popupWindow = Popup(title="Calendar", size_hint=(None, None), size=(300, 380))
+    glay = GridLayout(cols=7, size_hint = (1, 1))
+    arr = ["S", "M", "Tu", "W", "Th", "F", "S"]
+    for i in range(7): 
+        glay.add_widget(Label(text=arr[i]))
+    for __ in range(49): 
+        btn = NewBtn(text="t", background_color= (0,0,0,0.5))
+        btn.bind(on_press = btn.changebg)
+        glay.add_widget(btn)
+    popupWindow.add_widget(glay)
 
-    popupWindow = Popup(title="Calendar", content=show, size_hint=(None, None), size=(300, 380))
-    
     popupWindow.open()
 
 class CreateApp(App):
