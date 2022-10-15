624232606b35f1b6b436e9de50dfadc0127a75c3
Hanna Koh <hjk5643@g.rit.edu>
2022-10-15 01:16:38 -0400

    messed with styling

--- loginscreen.py
+++ loginscreen.py
@@ -1,17 +1,13 @@
-from cgitb import text
-from tkinter import CENTER, Grid
-from typing import Text
-from kivy.app import App
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.widget import Widget
 
-from kivy.config import Config
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.properties import ListProperty
 from kivy.core.window import Window
 Window.size = (480, 853)
+
+class SignupButton: 
+    b_color = ListProperty()
 
 class Login(Widget):
     pass
