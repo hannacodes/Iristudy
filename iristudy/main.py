ee827b73d06b5375826927d4dfd4e91329dae755
Hanna Koh <hjk5643@g.rit.edu>
2022-10-15 12:07:05 -0400

    messing with mygroup

--- iristudy/main.py
+++ iristudy/main.py
@@ -5,7 +5,13 @@ from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.screenmanager import ScreenManager, Screen
+
+#from mygroups import mygroups
 
 class WindowManager(ScreenManager):  
     pass
@@ -26,10 +32,23 @@ class Account(Screen):
     def __init__(self, **kwargs):  
         super().__init__(**kwargs)
 
-class mygroups(Screen):
+class mygroups(Screen): 
     kv = Builder.load_file("mygrp.kv")
     def __init__(self, **kwargs):  
         super().__init__(**kwargs)
+
+class Scroll(ScrollView):
+    def __init__(self,  **kwargs):
+        super(Scroll, self).__init__(**kwargs)
+        layout = GridLayout(cols=1, spacing=20, pos_hint ={'x':0, 'y': 0.5}, size_hint_y = None)
+        layout.bind(minimum_height=layout.setter('height'))
+        # Make sure the height is such that there is something to scroll.
+        for i in range(15):
+            SkillStat = BoxLayout(height = 80, size_hint_y = None, orientation = "vertical")
+            SkillStat.add_widget(Label(text = "Name\nSubject\nAdmin", color = (0,0,0,1), size_hint_y = None, pos_hint = {"center_x":.5, "top":0}))
+            SkillStat.add_widget(Button(text = "more info", size_hint = (.4, .2), pos_hint = {"center_x": .5}))
+            layout.add_widget(SkillStat)
+        self.add_widget(layout)
 
 class Profile(Screen):
     kv = Builder.load_file("profile.kv")
@@ -56,7 +75,7 @@ class SearchGroupScreen(Screen):
 
 sm = WindowManager() 
 
-screens = [Login(name="login"), Account(name="AccountApp"), Homepage(name="home"), Profile(name="profile"), CreateFormScreen(name='create'), SearchGroupScreen(name='search')]  
+screens = [Login(name="login"), Account(name="AccountApp"), Homepage(name="home"), Profile(name="profile"), CreateFormScreen(name='create'), SearchGroupScreen(name='search'), mygroups(name="mygroups")]  
 
 for screen in screens:  
     print("widget added")
--- iristudy/mygroups.py
+++ iristudy/mygroups.py
@@ -11,31 +11,40 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.screenmanager import Screen
 from kivy.graphics import *
 
 class MyRelativeLayout(RelativeLayout):
     def adjust_size(self, *args):
         self.rect.size = self.size  # set the size of the Rectangle
 
+
+
+
 class Scroll(ScrollView):
     def __init__(self,  **kwargs):
         super(Scroll, self).__init__(**kwargs)
-        layout = GridLayout(cols=1, spacing=20, pos_hint ={'x':0, 'y': 0.5}, size_hint_y = None)
+        layout = GridLayout(cols=1, spacing=10, pos_hint ={'x':0, 'y': 0}, size_hint_y = None, height = "0.7")
         layout.bind(minimum_height=layout.setter('height'))
         # Make sure the height is such that there is something to scroll.
         for i in range(15):
-            SkillStat = BoxLayout(height = 80, size_hint_y = None, orientation = "vertical")
-            SkillStat.add_widget(Label(text = "Name\nSubject\nAdmin", color = (0,0,0,1), size_hint_y = None, pos_hint = {"center_x":.5, "top":0}))
-            SkillStat.add_widget(Button(text = "more info", size_hint = (.4, .2), pos_hint = {"center_x": .5}))
+            #SkillStats = BoxLayout(size_hint_y = None, orientation = "vertical")
+            SkillStat = BoxLayout(spacing = 10, height = 200, orientation = "vertical", size_hint_y = None)
+            SkillStat.add_widget(Label(text = "Name\nSubject\nAdmin", color = (0,0,0,1), size_hint = (None, None), size = (80, 50), pos_hint = {"center_x":.5, "top":0}))
+            flaylout = FloatLayout(size_hint = (1, 1))
+            flaylout.add_widget(Button(text = "more info", size_hint = (0.3, 0.4), pos_hint = {"center_x": .5}))
+            SkillStat.add_widget(flaylout)
             layout.add_widget(SkillStat)
+
         self.add_widget(layout)
 
 class scrollingtest(BoxLayout):
     pass
             
 
-class mygroups(Widget): 
+class mygroups(Screen): 
     pass 
 
 class mygrpApp(App):
