730b5ec47323e923f036c2b7ecc452a5e63c1df5
tusmm <r.ong40915@gmail.com>
2022-10-14 22:35:42 -0400

    initial search group setup

--- searchgroup.py
+++ searchgroup.py
@@ -1,3 +1,25 @@
 """
 Search group screen
 """
+
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label # writing
+from kivy.uix.gridlayout import GridLayout # table
+from kivy.uix.textinput import TextInput # input text
+from kivy.uix.button import Button # button component
+from kivy.uix.widget import Widget
+from kivy.uix.dropdown import DropDown
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+Window.size = (480/1.5, 853/1.5)
+
+class SearchGroupScreen(Widget):
+    pass
+
+class SearchApp(App):
+    def build(self):
+        return SearchGroupScreen()
+
+if __name__ == '__main__':
+    SearchApp().run()
