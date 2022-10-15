73bc29730c7be084d85d77efeddd1a3e8c41febf
lh5844 <lh5844rit@gmail.com>
2022-10-15 14:37:53 -0400

    testing stuff in homepage with relative layout

--- iristudy/homepage.py
+++ iristudy/homepage.py
@@ -1,18 +1,26 @@
+from webbrowser import BackgroundBrowser
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.core.window import Window
 from kivy.utils import *
+from kivy.graphics import *
 
 Window.size = (480/1.5, 853/1.5)
+
+class infoLabel(Label):
+    pass
+
 
 class Scroll(ScrollView):
     def __init__(self,  **kwargs):
@@ -20,11 +28,24 @@ class Scroll(ScrollView):
         layout = GridLayout(cols=1, spacing=1, pos_hint ={'x':0, 'y': 0}, size_hint_y = None, height = "0.7")
         layout.bind(minimum_height=layout.setter('height'))
         # Make sure the height is such that there is something to scroll.
+        
         for i in range(15):
             SkillStat = BoxLayout(spacing = 2, height = 150, orientation = "vertical", size_hint_y = None)
-            SkillStat.add_widget(Label(text = "Name", color = (0,0,0,1), size_hint = (None, None), size = (80, 20), pos_hint = {"center_x":.5, "top":0}, font_name = 'assets/fonts/static/Fredoka/Fredoka-regular'))
+            BackgroundBox = RelativeLayout()
+            test = infoLabel(size = (200, 100))
+            BackgroundBox.add_widget(test)
+            layout.add_widget(BackgroundBox)
+
+            name = Label(text="Name",color = (0,0,0,1), size_hint = (None, None), size = (80, 20), pos_hint = {"center_x":.5, "top":0}, font_name = 'assets/fonts/static/Fredoka/Fredoka-regular')
+            #with name.canvas.before:
+             #   Color(0, 1, 0, 0.25)
+              #  Rectangle(pos= (100, 200), size = self.size)
+            SkillStat.add_widget(name)
+
+            #SkillStat.add_widget(Label(text = "Name", color = (0,0,0,1), size_hint = (None, None), size = (80, 20), pos_hint = {"center_x":.5, "top":0}, font_name = 'assets/fonts/static/Fredoka/Fredoka-regular'))
             SkillStat.add_widget(Label(text = "Subject", color = (0,0,0,1), size_hint = (None, None), size = (80, 20), pos_hint = {"center_x":.5, "top":0}, font_name = 'assets/fonts/static/Fredoka/Fredoka-regular'))
             SkillStat.add_widget(Label(text = "Admin", color = (0,0,0,1), size_hint = (None, None), size = (80, 20), pos_hint = {"center_x":.5, "top":0}, font_name = 'assets/fonts/static/Fredoka/Fredoka-regular'))
+            
             flaylout = FloatLayout(size_hint = (1, 1))
             flaylout.add_widget(Button(text = "more info", size_hint = (0.3, 0.4), pos_hint = {"center_x": .5, "top": 0.9}, font_name = 'assets/fonts/static/Fredoka/Fredoka-regular', background_color = get_color_from_hex('#BF98D1'), background_normal = ''))
             SkillStat.add_widget(flaylout)
