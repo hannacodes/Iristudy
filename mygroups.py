7d7dd98a01c2d398e0978c70dcaf58e8be24ed4e
lh5844 <lh5844rit@gmail.com>
2022-10-14 22:49:41 -0400

    added homepage label on top with app name

--- mygroups.py
+++ mygroups.py
@@ -1,1 +1,17 @@
 
+import kivy
+
+from kivy.core.window import Window
+Window.size = (480/1.5, 853/1.5)
+
+from kivy.app import App 
+from kivy.uix.widget import Widget 
+
+class mygroups(Widget): 
+    pass 
+
+class mygrpApp(App):
+    def build(self):
+        return mygroups()
+    
+mygrpApp().run()
