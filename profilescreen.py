3eeb5010ac00ce86ba6e4c6491fd704d90b7b944
Hanna Koh <hjk5643@g.rit.edu>
2022-10-15 04:32:35 -0400

    fixed nav

--- profilescreen.py
+++ profilescreen.py
@@ -3,7 +3,7 @@ from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
-Window.size = (480, 853)
+Window.size = (480/1.5, 853/1.5)
 
 class Profile(Screen):
     Builder.load_file("profile.kv")
