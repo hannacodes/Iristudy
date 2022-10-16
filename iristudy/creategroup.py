d13bae7a0d0ffc1ee5bf28b85a1360dd954be018
Hanna Koh <hjk5643@g.rit.edu>
2022-10-15 22:12:47 -0400

    changed spinner font

--- iristudy/creategroup.py
+++ iristudy/creategroup.py
@@ -32,7 +32,7 @@ class CreateFormScreen(Screen):
         show_popup()
 
     def spinner_clicked(self, value):
-        self.subject.text = value;
+        self.subject.text = value
 
     def submit(self):
         # self.add_widget(Label(text=f'{self.groupName.text}, {self.description.text}'))
